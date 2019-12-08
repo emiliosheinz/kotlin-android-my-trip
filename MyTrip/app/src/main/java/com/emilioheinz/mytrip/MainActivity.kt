7c339ec0df08d704505e5c9710462d46d779ebf9
emilioheinz <emiliosheinz@gmail.com>
2019-12-08 14:48:06 -0300

    Handling on calculate click

--- MyTrip/app/src/main/java/com/emilioheinz/mytrip/MainActivity.kt
+++ MyTrip/app/src/main/java/com/emilioheinz/mytrip/MainActivity.kt
@@ -2,13 +2,27 @@ package com.emilioheinz.mytrip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(view: View) {
+        val id = view.id
+
+        if(id == R.id.buttonCalculate) {
+            handleCalculateClick()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        buttonCalculate.setOnClickListener(this)
+    }
+
+    private fun handleCalculateClick() {
 
     }
 }
