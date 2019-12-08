99e2ff20c2f96dfabe201a535205ff9dadfa7c5a
emilioheinz <emiliosheinz@gmail.com>
2019-12-08 15:31:26 -0300

    Calculating total trip price

--- MyTrip/app/src/main/java/com/emilioheinz/mytrip/MainActivity.kt
+++ MyTrip/app/src/main/java/com/emilioheinz/mytrip/MainActivity.kt
@@ -3,7 +3,9 @@ package com.emilioheinz.mytrip
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -23,6 +25,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun handleCalculateClick() {
+        if(isValid()) {
+            try {
+                val distance = editDistance.text.toString().toFloat()
+                val price = editPrice.text.toString().toFloat()
+                val autonomy = editAutonomy.text.toString().toFloat()
 
+                val result = ((distance * price) / autonomy)
+
+                textResult.setText("Total: R$ $result")
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(this, R.string.invalid_values, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(this, R.string.invalid_values, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isValid(): Boolean {
+        val isDistanceValid = editDistance.text.toString() != ""
+        val isPriceValid = editPrice.text.toString() != ""
+        val isAutonomyValid = editAutonomy.text.toString() != ""  && editAutonomy.text.toString() != "0"
+
+        return isDistanceValid && isPriceValid && isAutonomyValid
     }
 }
